1ef0c6d389ecc7e2b64e61435b486abcc840dbbc
Soledad Penades <sole@mozilla.com>
2013-12-06 00:38:43 +0000

    push_notifications example - client posting channel to own app server

---
+++ examples/push_notifications/client/js/main.js
@@ -0,0 +1,93 @@
+(function() {
+    var errorDiv;
+    var server = 'http://localhost:6666';
+
+    if( !navigator.push ) {
+        window.alert('Push Notifications are not available in this environment');
+    } else {
+        init();
+    }
+
+    // ---
+
+    function init() {
+        console.log('init');
+
+        // UI
+        errorDiv = document.getElementById('error');
+        serverDiv = document.getElementById('server');
+
+        serverDiv.value = server;
+
+        serverDiv.addEventListener('change', updateServerPath, false);
+        serverDiv.addEventListener('keyup', updateServerPath, false);
+
+        // Register with the Push Notification Server
+        var request = navigator.push.register();
+
+        request.onsuccess = function(ev) {
+
+            // Get channel URL
+            var channelURL = request.result;
+            console.log(ev);
+            console.log('channelURL ' + channelURL);
+
+            // Let our server know about the private channelURL
+            ajax('POST', '/register', {
+                channelURL: channelURL
+            }, function(err, res) {
+                if(err) {
+                    reportError('Unable to register channel: ' + err);
+                } else {
+                    
+                }
+            });
+        };
+
+        request.onerror = function(ev) {
+            reportError('Unable to connect to push notification server');
+        };
+        
+    }
+
+
+    function updateServerPath() {
+        server = serverDiv.value.trim();
+        console.log('update to', server);
+    }
+
+
+    function ajax(method, path, data, callback) {
+
+        var url = server + path;
+        var xhr = new XMLHttpRequest({ mozSystem: true });
+
+        xhr.onload = function (evt) {
+            if (xhr.status === 200 || xhr.status === 0) {
+                callback(null, xhr.response);
+            } else {
+                callback(xhr.status);
+            }
+        };
+
+        xhr.open(method, url, true);
+        xhr.onerror = function(e) {
+            console.error('xhr error ' + xhr.status);
+            callback(e);
+        };
+
+        var formData = new FormData();
+        Object.keys(data).forEach(function(k) {
+            formData.append(k, data[k]);
+        });
+
+        xhr.send(formData);
+
+    }
+
+
+    function reportError(msg) {
+        errorDiv.innerHTML += msg + '<br />';
+    }
+
+})();
