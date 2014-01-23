c192bf891fd9a4381cb72f6e0bd64f89d6f389b7
Soledad Penades <sole@mozilla.com>
2014-01-23 17:06:24 +0000

    urgh, delete old code O:-)

--- examples/orientation_lock/main.js
+++ examples/orientation_lock/main.js
@@ -1,38 +1,5 @@
 window.onload = function() {
     
-/*    if( !window.MozActivity ) {
-        alert('This environment does not support Web Activities.\nSorry about that :-(\nMaybe try it on Firefox OS?');
-        return;
-    }
-
-    var pictures = document.getElementById('pictures');
-    var button = document.querySelector('input[type=button]');
-
-    button.addEventListener('click', launchActivity, false);
-
-    function launchActivity() {
-
-        var activity = new MozActivity({
-            name: 'pick',
-            data: {
-                type: [ 'image/jpeg', 'image/jpg', 'image/png' ]
-            }
-        });
-
-        activity.onsuccess = function() {
-            var picture = this.result;
-            var img = document.createElement('img');
-            img.src = window.URL.createObjectURL(this.result.blob);
-            pictures.appendChild(img);
-        };
-
-        activity.onerror = function() {
-            alert(this.error);
-        };
-
-    }
-*/
-
     var btnPortrait = document.getElementById('portrait');
     var btnLandscape = document.getElementById('landscape');
 
